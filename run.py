37477e0dac90eb541f80e67c02156c2732d896d3
henrikauestad <henrik.stormark.auestad@gmail.com>
2021-09-01 09:49:20 +0200

    lost scripts

---
+++ run.py
@@ -0,0 +1,14 @@
+# import scripts.Henrik.process_norkyst
+# import scripts.Henrik.verify2norkyst2
+# import scripts.Henrik.compare_rho
+# import scripts.Henrik.era_variance
+# import scripts.Henrik.norkyst_variance
+
+# import scripts.Henrik.wind_against_ERA
+# import scripts.Henrik.skill_on_norkyst_CY47R2
+# import scripts.Henrik.correlation_norkyst_bw
+# import scripts.Henrik.skill_on_era2
+
+import scripts.Henrik.hardangerfjorden
+# import scripts.Henrik.hele_kysten
+# import scripts.Henrik.hisdalen_obs
