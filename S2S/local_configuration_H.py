13b5507c26f98eab183dd29d3cea4d594b902598
Henrik Auestad <heau@login2-nird-tos.nird.login.svc.cluster.local>
2021-09-01 10:44:07 +0200

    Config file that works on the new node. Not user specific.

--- S2S/local_configuration_H.py
+++ S2S/local_configuration_H.py
@@ -1,18 +1,18 @@
 config = {
-    "BW_DIR": '/nird/projects/NS9001K/sso102/Python/TOOLS/S2S-download/data/BW', # "/nird/projects/NS9001K/sso102/Python/S2S-download/Benjamin",
+    "BW_DIR": '/projects/NS9001K/sso102/Python/TOOLS/S2S-download/data/BW', # "/nird/projects/NS9001K/sso102/Python/S2S-download/Benjamin",
     "CF_DATA_DIR": '', # "/nird/projects/NS9001K/sso102/Python/S2S-download/Benjamin",
-    "S2S_DIR": '/nird/projects/NS9853K/DATA/S2S', # "/nird/projects/NS9853K/DATA/S2S",
+    "S2S_DIR": '/projects/NS9853K/DATA/S2S', # "/nird/projects/NS9853K/DATA/S2S",
     "S2SEUR_DIR": '', # "/nird/projects/NS9001K/sso102/S2S/DATA/grib/",
     "SFE_DIR": '', # "/nird/projects/NS9853K/DATA/SFE/Systems_daily_grib/",
     "ERA5_daily_DIR": '', # "/nird/projects/NS9853K/DATA/SFE/ERA_daily_nc/",
     "ERA5_monthly_DIR": '', # "/nird/projects/NS9853K/DATA/SFE/ERA_monthly_nc/",
     "S2S_DIR_summer2018": '', # "/nird/projects/NS9001K/sso102/S2S/DATA/SUMMER2018",
     "SAVEFIG": 'figures/', # " /nird/projects/NS9001K/sso102/FIGUERS"
-    'VALID_DB':'data/valid_db/',
-    'NORKYST':'data/norkyst800/',
-    'S2SF':'/nird/projects/NS9853K/DATA/S2S/forecast/ECMWF/sfc/',
-    'S2SH':'/nird/projects/NS9853K/DATA/S2S/hindcast/ECMWF/sfc/',
-    'ERA5':'/nird/projects/NS9853K/DATA/SFE/ERA_daily_nc/grid_15/',
+    'VALID_DB':'/projects/NS9853K/DATA/processed/valid_db/',
+    'NORKYST':'/projects/NS9853K/DATA/norkyst800/',
+    'S2SF':'/projects/NS9853K/DATA/S2S/forecast/ECMWF/sfc/',
+    'S2SH':'/projects/NS9853K/DATA/S2S/hindcast/ECMWF/sfc/',
+    'ERA5':'/projects/NS9853K/DATA/SFE/ERA_daily_nc/grid_15/',
     # 'S2SF':'data/nird_copy/S2S/Benjamin/forecast/ECMWF/sfc/', # local to henrik
     # 'S2SH':'data/nird_copy/S2S/Benjamin/hindcast/ECMWF/sfc/', # local to henrik
     # 'ERA5':'data/nird_copy/SFE/ERA_daily_nc/grid_15/',  # local to henrik
@@ -21,7 +21,7 @@ config = {
     'BW':'./data/BW/BW_temperature/',
     'IMR':'./data/nird_copy/IMR/fastestasjoner/',
     'EIDE':'./projects/SST_oppdrett/',
-    'S2SH_HR':'/nird/projects/NS9853K/DATA/S2S/MARS/hindcast/ECMWF/sfc/',
-    'ERA5_HR':'/nird/projects/NS9853K/DATA/SFE/ERA_daily_nc/',
+    'S2SH_HR':'/projects/NS9853K/DATA/S2S/MARS/hindcast/ECMWF/sfc/',
+    'ERA5_HR':'/projects/NS9853K/DATA/SFE/ERA_daily_nc/',
     'MPL_STYLE':'./S2S/graphics/s2s.mplstyle'
 }
