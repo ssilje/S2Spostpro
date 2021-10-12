722172a2a411bafa02b7be96333c54608c53ad32
ssilje <sso102@login3-nird-tos.nird.login.svc.cluster.local>
2021-10-12 11:00:11 +0200

    adding scripts for confer

---
+++ scripts/Silje/CONFER/ENSEMBLE_Design.py
@@ -0,0 +1,21 @@
+import numpy as np
+import xarray as xr
+import pandas as pd
+import matplotlib.pyplot as plt
+import xskillscore as xs
+import time as time_lib
+import properscoring as ps
+from datetime import date, datetime, timedelta, timezone
+
+
+
+import csv
+
+file = open('Salary_Data.csv')
+
+csvreader = csv.reader(file)
+
+
+
+
+
