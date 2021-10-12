bebff1511b10e2ead596450b178401a25775ce0b
ssilje <sso102@login3-nird-tos.nird.login.svc.cluster.local>
2021-10-12 11:01:48 +0200

    adding scripts for confer

--- scripts/Silje/CONFER/ENSEMBLE_Design.py
+++ scripts/Silje/CONFER/ENSEMBLE_Design.py
@@ -11,11 +11,36 @@ from datetime import date, datetime, timedelta, timezone
 
 import csv
 
-file = open('Salary_Data.csv')
 
-csvreader = csv.reader(file)
+def read_csv(data_dir,
+             csv_name):
+    file = open(data_dir + '/' + csv_name)
+    csvreader = csv.reader(file)
+    header = next(csvreader)
+    print(header)
+    csv_file = []
+    for row in csvreader:
+            csv_file.append(row)
+
+    file.close()
+
+    return csv_file
 
 
+IODfcs = read_csv(data_dir = '/nird/projects/NS9853K/DATA/ENS_DESIGN',
+             csv_name = 'IOD_fcs.csv')
+
+
+IODdt = read_csv(data_dir = '/nird/projects/NS9853K/DATA/ENS_DESIGN',
+             csv_name = 'IOD_dt.csv')
+
+OBSdt = read_csv(data_dir = '/nird/projects/NS9853K/DATA/ENS_DESIGN',
+             csv_name = 'obs_dt.csv')
+precdt = read_csv(data_dir = '/nird/projects/NS9853K/DATA/ENS_DESIGN',
+             csv_name = 'prec_dt.csv')
+
+##data_dir = '/nird/projects/NS9853K/DATA/ENS_DESIGN'
+#csv_name = 'IOD_fcs.csv'
 
 
 
