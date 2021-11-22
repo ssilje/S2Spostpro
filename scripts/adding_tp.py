1ff143bb7a66d3f1c5d4cc2d497a618f3207da08
Silje Sørland <ssor@norceresearch.no>
2021-11-22 10:26:39 +0100

    Update adding_tp.py

--- scripts/adding_tp.py
+++ scripts/adding_tp.py
@@ -1,1 +1,54 @@
-test
+
+import pandas as pd
+import xarray as xr
+import xskillscore as xs
+import cartopy.crs as ccrs
+import matplotlib.pyplot as plt
+import numpy as np
+
+import seaborn as sns
+
+from S2S.data_handler import ERA5
+#from S2S.process import Hindcast, Observations, Forecast, Observations_hcfc
+from S2S.process import Hindcast, Observations, Forecast
+
+from S2S.graphics import mae,crps,graphics as mae,crps,graphics
+from S2S import models
+import S2S.xarray_helpers    as xh
+from S2S.scoring import uncentered_acc, centered_acc
+from S2S.local_configuration import config
+
+
+bounds          = (0,28,55,75)
+var             = 'tp'
+
+clabel          = 'K'
+t_start         = (2018,3,1)
+t_end           = (2018,5,1)
+
+
+clim_t_start    = (1999,1,1)
+clim_t_end      = (2019,12,31)
+
+
+high_res        = False
+
+steps           = pd.to_timedelta([7, 14, 21, 28, 35, 42],'D')
+
+
+print('\tProcess Hindcast')
+
+grid_hindcast = Hindcast(
+                        var,
+                        t_start,
+                        t_end,
+                        bounds,
+                        high_res=high_res,
+                        steps=steps,  
+                        process=False,
+                        download=False,
+                        split_work=False
+                    )
+
+era = ERA5(high_res=high_res)\
+                            .load(var,clim_t_start,clim_t_end,bounds)[var]
